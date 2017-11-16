5e7f5b4508ce6a68f42a0fa0b5c90676580fd72b
magomedgadjiev <magomedalievic@gmail.com>
2017-11-16 12:20:03 +0300

    update

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,19 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include <queue>
 #include <boost/python.hpp>
-#include <boost/python/list.hpp>
-#include <boost/python/extract.hpp>
 
 
-char const* bubbleSort(char* line, int size){
-
-    int temp; // временная переменная для обмена элементов местами
-
+char const* bubbleSort(char* line, int size)
+    int temp;
     for (int i = 0; i < size - 1; i++) {
        for (int j = 0; j < size - i - 1; j++) {
             if (line[j] > line[j + 1]) {
-                // меняем элементы местами
                 temp = line[j];
                 line[j] = line[j + 1];
                 line[j + 1] = temp;
@@ -24,7 +16,7 @@ char const* bubbleSort(char* line, int size){
 }
 
 
-BOOST_PYTHON_MODULE(sort_from_files)
+BOOST_PYTHON_MODULE(bubbleSort)
 {
     using namespace boost::python;
     def("bubbleSort", bubbleSort);
